aa11f5f776401dbc349faaffbe0742d218086095
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-12 03:19:05 +0600

    route set up, navbar and banner added

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-
+            <Navbar></Navbar>
+            <div className='min-h-screen'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,28 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctorTheme: {
+
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#191D24",
+
+          "base-100": "#ffffff"
+
+        },
+      },
+    ],
+  },
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [require("daisyui")],
 }
