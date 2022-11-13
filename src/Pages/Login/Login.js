a16ddc407dcb708f3553f841ff9ac8bdada3a08d
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-14 02:48:46 +0600

    sign up  form created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="text"
+                        <input type="email"
                             className="input input-bordered w-full max-w-xs"
                             {...register("email",
                                 {
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleSignUp = (data) => {
+        console.log(data);
+        console.log(errors);
+    }
+
+    return (
+        <div className='h-[800px]  flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleSignUp)} >
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text"
+                            {...register("name", {
+                                required: "Name is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email"
+                            {...register("email", {
+                                required: "Email is required"
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 character or longer' },
+                                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                        <label className="label">
+                            <span className="label-text">Forget Password</span>
+                        </label>
+                    </div>
+                    <input className='btn btn-accent w-full ' value="Sign Up" type="submit" />
+                </form>
+                <p>Already have an account? <Link className='text-secondary' to='/login'>please login</Link></p>
+                <div className='divider'>OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
