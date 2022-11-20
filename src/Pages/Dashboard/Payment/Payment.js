a78ce8e67f4adc84a9400527cb2da7a8366fcbf6
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-20 17:27:37 +0600

    stripe intregated

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,11 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 
 const Payment = () => {
     const booking = useLoaderData();
@@ -8,6 +14,11 @@ const Payment = () => {
         <div>
             <h3 className='text-3xl'>Payment For {treatment}</h3>
             <p className='text-3xl'>Please pay ${price} for your appointment on {appointmentDate} at {slot}</p>
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
